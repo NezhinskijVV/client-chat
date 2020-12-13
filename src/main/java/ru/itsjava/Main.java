45835d5e40bbcccd38749f46b18fe31d8f28a81b
Vitaliy <SeekerOfConfessor@gmail.com>
2020-12-13 20:27:00 +0300

    listen server

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -1,13 +1,14 @@
 package ru.itsjava;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Main {
     public final static int PORT = 8081;
     public final static String HOST = "localhost";
-
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket(HOST, PORT);
@@ -16,6 +17,11 @@ public class Main {
             PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
             serverWriter.println("Hi from client!");
             serverWriter.flush();
+
+            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (true){
+                System.out.println(serverReader.readLine());
+            }
         }
 
     }
