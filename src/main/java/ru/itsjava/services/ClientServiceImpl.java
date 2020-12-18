6f648b1324a888a581c2a95341979cf5d77a9536
Vitaliy <SeekerOfConfessor@gmail.com>
2020-12-18 11:56:12 +0300

    add authorization

--- src/main/java/ru/itsjava/services/ClientServiceImpl.java
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -2,6 +2,8 @@ package ru.itsjava.services;
 
 import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -20,11 +22,22 @@ public class ClientServiceImpl implements ClientService {
             MessageInputService messageInputService =
                     new MessageInputServiceImpl(System.in);
 
-            System.out.println("Введите сообщение");
-            String consoleMessage = messageInputService.getMessage();
+            System.out.println("Введите свой логин:");
+            String login = messageInputService.getMessage();
 
-            serverWriter.println(consoleMessage);
+            System.out.println("Введите свой пароль:");
+            String password = messageInputService.getMessage();
+
+//            !autho!login:password
+            serverWriter.println("!autho!" + login + ":" + password);
             serverWriter.flush();
+
+            while (true) {
+                String consoleMessage = messageInputService.getMessage();
+
+                serverWriter.println(consoleMessage);
+                serverWriter.flush();
+            }
         }
     }
 }
